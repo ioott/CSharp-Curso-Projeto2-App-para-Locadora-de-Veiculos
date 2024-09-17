912ca5ae302f524be916d9b01e6af6ec7f909d75
Vania Ioott <vioott@gmail.com>
2024-09-16 21:51:17 -0300

    feat(structs): adicionar struct Color com campos públicos Name e Hex

--- src/RentCars/Types/Structs/Color.cs
+++ src/RentCars/Types/Structs/Color.cs
@@ -1,6 +1,19 @@
-namespace RentCars.Types;
+namespace RentCars.Types
 
-//1 - Crie uma `Struct` para as **Cores**
-public struct Color
 {
+    public struct Color
+    {
+        // Nome da cor
+        public string Name { get; set; }
+
+        // Código hexadecimal da cor
+        public string Hex { get; set; }
+
+        // Construtor opcional para facilitar a inicialização
+        public Color(string name, string hex)
+        {
+            Name = name;
+            Hex = hex;
+        }
+    }
 }
