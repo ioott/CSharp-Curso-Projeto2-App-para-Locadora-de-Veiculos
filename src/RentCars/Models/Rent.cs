b02355740e201678871e3352e03e369ff436392b
Vania Ioott <vioott@gmail.com>
2024-09-16 23:13:52 -0300

    feat: implementa construtor de Rent e métodos Cancel e Finish com regras de negócio

--- src/RentCars/Models/Rent.cs
+++ src/RentCars/Models/Rent.cs
@@ -13,18 +13,34 @@ public class Rent
     //10 - Crie o construtor de `Rent` seguindo as regras de negócio
     public Rent(Vehicle vehicle, Person person, int daysRented)
     {
-        throw new NotImplementedException();
+        Vehicle = vehicle;
+        Person = person;
+        DaysRented = daysRented;
+
+        // Cálculo do preço
+        if (person is LegalPerson)
+        {
+            Price = vehicle.PricePerDay * daysRented * 0.9; // Desconto de 10% para PJ
+        }
+        else
+        {
+            Price = vehicle.PricePerDay * daysRented; // Preço normal para PF
+        }
+
+        Status = RentStatus.Confirmed; // Status inicial
+        Vehicle.IsRented = true; // Veículo alugado
+        Person.Debit += Price; // Atualiza o débito da pessoa
     }
 
     //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
     public void Cancel()
     {
-        throw new NotImplementedException();
+        Status = RentStatus.Canceled;
     }
 
     //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
     public void Finish()
     {
-        throw new NotImplementedException();
+        Status = RentStatus.Finished;
     }
 }
